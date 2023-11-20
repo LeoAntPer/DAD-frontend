2bd79641138ec93d7e1c6b04b572c805ed4c20c1
Leandro Pereira <2211053@my.ipleiria.pt>
2023-11-20 15:43:10 +0000

    Created Vue with source code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DAD-frontend
+# vue
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import './assets/main.css'
+import axios from 'axios'
+import { io } from 'socket.io-client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+const apiDomain = import.meta.env.VITE_API_DOMAIN
+const wsConnection = import.meta.env.VITE_WS_CONNECTION
+
+app.provide('socket', io(wsConnection))
+
+app.provide(
+  'axios',
+  axios.create({
+    baseURL: apiDomain + '/api',
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+)
+app.provide('serverBaseUrl', apiDomain)
 
 app.mount('#app')
