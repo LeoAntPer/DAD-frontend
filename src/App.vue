13190363e67ce6ae17ee96d74b9bcddcc854656c
Chibinha <47814243+Chibinha@users.noreply.github.com>
2023-12-06 11:32:57 +0000

    Dashboard - mudança do nome e icon

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,8 @@ const clickMenuOption = () => {
   <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top flex-md-nowrap p-0 shadow">
     <div class="container-fluid">
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-        <img src="@/assets/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-        App name
+        <img src="@/assets/logo.png" alt="" width="40" height="40" class="d-inline-block ">
+        VCard
       </a>
       <button id="buttonSidebarExpandId" class="navbar-toggler" type="button" data-bs-toggle="collapse"
         @click="clickMenuOption" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
@@ -47,7 +47,6 @@ const clickMenuOption = () => {
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
               data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="@/assets/avatar-exemplo-1.jpg" class="rounded-circle z-depth-0 avatar-img" alt="avatar image">
               <span class="avatar-text">User Name</span>
             </a>
             <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
